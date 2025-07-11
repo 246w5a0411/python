12c5ef5769ee0e580dd6e4d88a7156a7c5310359
246w5a0411 <skoppula70@gmail.com>
2025-07-11 11:28:46 +0530

    Add files via upload

---
+++ concatination operator.py
@@ -0,0 +1,15 @@
+#THIS OPERATOR USED TO CONCATINATION or COMBINE THE VALUES(+)
+mytuplem=(11,12,13)
+mytuplen=(14,15,16)
+p=mytuplem+mytuplen
+print(p)
+#*this operator used to repeat a given values
+mytuplem=(1,2,3)
+mytuplen=mytuplem*3
+print(mytuplen)
+#len: iT defines length of the tuple
+s=(1,2,3)
+print(len(s))
+#countof - it count the number of repeated values in te given sequence
+b=(11,12,13,11,14,15,11,16)
+print(b.count(11))
